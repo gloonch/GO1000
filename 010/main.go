1b1f04b51c93b2a1296e21dc3aabb2296095cfb7
gloonch <mahdihadian@hotmail.com>
2025-12-05 20:25:20 +0330

    0014 bouncing ball - done

--- 010/main.go
+++ 010/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
@@ -10,10 +13,17 @@ func main() {
 
 		cellEmpty = ' '
 		cellBall  = '⚾'
+
+		maxFrames = 1200 // 20 frames per second
+		speed     = time.Second / 20
 	)
 
-	var cell rune
-
+	var (
+		cell      rune
+		py, px    int
+		velocityX = 1
+		velocityY = 1
+	)
 	// create a board with width rows and height columns
 	// each cell has to be initialized with a byte
 	board := make([][]bool, width)
@@ -21,18 +31,51 @@ func main() {
 		board[row] = make([]bool, height)
 	}
 
-	board[0][0] = true
+	buf := make([]rune, 0, width*height)
 
-	// draw the board
-	for y := range board[0] {
-		for x := range board {
-			cell = ' '
-			if board[x][y] {
-				cell = cellBall
+	for i := 0; i < maxFrames; i++ {
 
-			}
-			fmt.Print(string(cell), " ")
+		// move the ball
+		px += velocityX
+		py += velocityY
+
+		if px >= width-1 || px <= 0 {
+			velocityX *= -1
 		}
-		fmt.Println()
+		if py >= height-1 || py <= 0 {
+			velocityY *= -1
+		}
+
+		// remove the previous ball position
+		for y := range board[0] {
+			for x := range board {
+				board[x][y] = false
+			}
+		}
+
+		// set the ball position
+		board[px][py] = true
+
+		buf = buf[:0]
+		// draw the board
+		for y := range board[0] {
+			for x := range board {
+				cell = ' '
+				if board[x][y] {
+					cell = cellBall
+
+				}
+				fmt.Print(string(cell), " ")
+				buf = append(buf, cell, ' ')
+			}
+			fmt.Println()
+			buf = append(buf, '\n')
+		}
+
+		fmt.Print("\033[2J")
+
+		fmt.Print(string(buf))
+
+		time.Sleep(speed)
 	}
 }
