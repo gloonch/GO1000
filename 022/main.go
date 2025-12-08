8b5dd9df292c8d045387ecee16393b0df4bce239
gloonch <mahdihadian@hotmail.com>
2025-12-08 19:52:37 +0330

    027 concurrency: atomic value struct

---
+++ 022/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type Person struct {
+	Name string
+	Age  int32
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	var person atomic.Value
+
+	person.Store(&Person{Name: "Flashitia", Age: 40})
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			p := person.Load().(*Person)
+			atomic.AddInt32(&p.Age, 1)
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println(person.Load().(*Person))
+}
