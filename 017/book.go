acfba96721aea1a3d392313998cda0a0caa83fdb
gloonch <mahdihadian@hotmail.com>
2025-12-08 07:22:31 +0330

    20 empty interface

--- 017/book.go
+++ 017/book.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type book struct {
-	title string
-	price money
+	title      string
+	price      money
+	publishing interface{}
 }
 
 func (b book) print() {
-	fmt.Printf("%-13s : %s\n", b.title, b.price.string())
+	p := format(b.publishing)
+	fmt.Printf("%-13s : %s - (%v)\n", b.title, b.price.string(), p)
+}
+
+func format(publishing interface{}) string {
+	if publishing == nil {
+		return "unknown"
+	}
+
+	var t int
+	if publishing, ok := publishing.(int); ok {
+		t = publishing
+	}
+	if publishing, ok := publishing.(string); ok {
+		t, _ = strconv.Atoi(publishing)
+	}
+
+	u := time.Unix(int64(t), 0)
+	return u.String()
+
 }
--- 017/main.go
+++ 017/main.go
@@ -2,15 +2,16 @@ package main
 
 func main() {
 	var (
-		minecraft = game{title: "minecraft", price: 20}
-		tetris    = game{title: "tetris", price: 10}
-		mobydick  = book{title: "tetris", price: 10}
-		rubik     = book{title: "rubik", price: 17}
-		yoda      = toy{title: "baby yoda", price: 0}
+		minecraft      = game{title: "minecraft", price: 20}
+		tetris         = game{title: "tetris", price: 10}
+		mobydick       = book{title: "moby dick", price: 10}
+		thegreatgatsby = book{title: "the great gatsby", price: 30, publishing: 118281600}
+		rubik          = toy{title: "rubik", price: 17}
+		yoda           = toy{title: "baby yoda", price: 0}
 	)
 
 	var store list
-	store = append(store, &minecraft, &tetris, &mobydick, rubik, &yoda)
+	store = append(store, &minecraft, &tetris, &mobydick, thegreatgatsby, rubik, &yoda)
 	store.discount(0.5)
 	store.print()
 
