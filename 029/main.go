7ed476f60ed9e75ca117b6558b36a43c83765b5c
gloonch <mahdihadian@hotmail.com>
2025-12-09 05:19:11 +0330

    035 concurrency: getting urls - statistics data

--- 029/main.go
+++ 029/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 type response struct {
@@ -17,15 +19,28 @@ type summarizedResponse struct {
 	occurrences int
 }
 
+type appStatistics struct {
+	successResponses atomic.Int64
+	failureResponses atomic.Int64
+	idleTime         atomic.Int64
+	requestTime      atomic.Int64
+	appTotalTime     atomic.Int64
+}
+
 var wg sync.WaitGroup
 var mu sync.Mutex
 var success, failure chan response
 var sumResponse []*summarizedResponse
+var statistics appStatistics
 
 func main() {
 
+	start := time.Now()
+
 	success = make(chan response)
 	failure = make(chan response)
+
+	statistics = appStatistics{}
 
 	urls := []string{
 		"https://google.com/not-found",
@@ -46,15 +61,22 @@ func main() {
 
 	go func() {
 		for {
+			startTime := time.Now()
 			select {
 			case success := <-success:
 				summarize(success)
+				statistics.successResponses.Add(1)
 				fmt.Println(success)
 				wg.Done()
 			case failure := <-failure:
 				summarize(failure)
+				statistics.failureResponses.Add(1)
 				fmt.Println(failure)
 				wg.Done()
+			default:
+				// idle time
+				idleTime := time.Since(startTime).Microseconds()
+				statistics.idleTime.Add(idleTime)
 
 			}
 		}
@@ -62,13 +84,28 @@ func main() {
 
 	wg.Wait()
 
-	fmt.Println("--- results ---")
+	statistics.appTotalTime.Add(time.Since(start).Microseconds())
+
+	fmt.Println("\n--- results ---")
 	for _, sum := range sumResponse {
 		fmt.Printf("results by code: %v\n", sum.code)
 	}
+
+	fmt.Println("\n--- statistics ---")
+	fmt.Printf("successes: %v\n", statistics.successResponses.Load())
+	fmt.Printf("failures: %v\n", statistics.failureResponses.Load())
+	fmt.Printf("idle time (sec): %v\n", float64(statistics.idleTime.Load())/1000/1000)
+	fmt.Printf("request time (sec): %v\n", float64(statistics.requestTime.Load())/1000/1000)
+	fmt.Printf("app total time (sec): %v\n", float64(statistics.appTotalTime.Load())/1000/1000)
+
 }
 
 func fetch(url string) {
+	start := time.Now()
+	defer func() {
+		requestTime := time.Since(start).Microseconds()
+		statistics.requestTime.Add(requestTime)
+	}()
 	res, err := http.Get(url)
 	if err != nil {
 		failure <- response{
