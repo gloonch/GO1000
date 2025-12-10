e599d47e24e725f009e71c2b891bc975d8d9339b
gloonch <mahdihadian@hotmail.com>
2025-12-10 06:23:29 +0330

    044 concurrency: webapp: init project

---
+++ 035 webapp/cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+const webPort = "80"
+
+func main() {
+
+	// database connection
+
+	// sessions
+
+	// channels
+
+	// waitgroup
+
+	// set up the application config
+
+	// set up mail
+
+	// listen for web connections
+
+}
